30928c80abe527bf97eaf6b8db4163835dad1398
DSdatsme <darsuratwala@gmail.com>
2020-03-28 01:23:01 +0530

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import express, { Router, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  app.get( "/filteredimage/", async( req: Request, res: Response ) => {
+    const image_url: string = req.query.image_url;
+
+    // image basic validation
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`image url is required`);
+    }
+
+    // compress user's image
+    const compressedImagePath: string = await filterImageFromURL(image_url);
+    
+    res.sendFile(compressedImagePath, () =>
+        deleteLocalFiles([compressedImagePath])
+      );
+  });
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
